af63d573fe16cc3331a1dfe5c828ded7d6fed0a6
Alex <alex.tubau@gmail.com>
2023-05-25 13:51:37 +0200

    Project converted from ts to js

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+
+export default defineComponent({
+    components: {
+    IonRouterOutlet,
+    IonApp,
+  },
+    setup() {
+    },
+})
+
+</script>
